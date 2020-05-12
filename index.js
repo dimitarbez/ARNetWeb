4de4dec1aedb8c490e23e7b068fd2821ebb227d7
Dimitar Bezhanovski <30288047+dimitarbez@users.noreply.github.com>
2020-05-12 23:21:44 +0200

    partials

---
+++ index.js
@@ -0,0 +1,23 @@
+let express = require('express');
+
+let app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/u/:user", (req, res) => {
+    let user = req.params.user;
+    res.render("home", {user: user});
+});
+
+app.get('*', (req, res) => {
+    res.send('Wrong route');
+});
+
+app.listen(3000, function() {
+    console.log('app started on port 3000');
+});
