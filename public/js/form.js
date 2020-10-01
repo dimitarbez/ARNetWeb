a98ea4696f55f1b1b8cadad1711487d9a7055a81
Dimitar Bezhanovski <30288047+dimitarbez@users.noreply.github.com>
2020-10-01 21:12:54 +0200

    added form js file

---
+++ public/js/form.js
@@ -0,0 +1,19 @@
+document.querySelector("#submit").disabled = true;
+
+document.querySelector('.custom-file-input').addEventListener('change',function(e){
+    var fileName = document.getElementById("file_to_upload").files[0].name;
+    var nextSibling = e.target.nextElementSibling
+
+    let extensionType = fileName.split(".").pop();
+    if (extensionType != "glb" && extensionType != "gltf") {
+        //alert("Model must be of GLB or GLTF type!");
+        document.querySelector("#file_to_upload").value = null;
+        nextSibling.innerText = "Choose a 3D model with the correct format!";
+        document.querySelector("#submit").disabled = true;
+    }
+    else {
+        nextSibling.innerText = fileName
+        document.querySelector("#submit").disabled = false
+    }
+    console.log(extensionType);
+})
